0e41930cd0e11d6fab54b4725b8f007b84544526
Novish Khalil <novishkhalil2@hotmail.com>
2024-11-11 00:32:02 +0500

    Add files via upload

---
+++ Q21.js
@@ -0,0 +1,9 @@
+"use strict";
+let user = {
+    fname: "Novish",
+    lname: "Rehan",
+    age: 33,
+    salary: 30000,
+    email: "abc@abc.com",
+};
+console.log(user);
---
+++ Q21.ts
@@ -0,0 +1,10 @@
+// 21.They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+type person = {fname:string,lname:string,age:number,salary:number,email:string}
+let user: person = {
+fname:"Novish",
+lname:"Rehan",
+age:33,
+salary:30000,
+email:"abc@abc.com",
+}
+console.log(user)
